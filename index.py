852a59bfb266d8d9ae82441a7575eaaf064f5887
Mayuradlak212 <156865950+Mayuradlak212@users.noreply.github.com>
2025-02-12 21:31:07 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,43 @@
+name ="Mayur Adlak "
+
+
+print(type( name))
+
+num=23
+bankBalance= 43.323
+marks =[5,6,7,9,9]
+person ={
+    "name":"Mayur Adlak",
+    "marks":marks,
+    "age":25,
+    "city":"Mumbai",
+    "makrs":[1,2,3,4]
+}
+# print("Person Keys : ",person.keys())
+
+point =(3,5,5,8,8)
+
+# print("Point : ",type(point))
+
+# print("Person Values : ",person.values())
+
+# Single Line Comment in Python 
+
+'''
+Multiline Comment in Python
+'''
+# print("Number ",type(num))
+
+# print("Marks ", type(marks))
+
+# print("person ", type(person))
+
+# print("Bank Balance ",type(bankBalance))
+
+data =set([4,6,7,8,3,1,8])
+
+print("Data Set : ",data)
+
+isActive =True
+
+print("Is Active : ", type(isActive), isActive)
